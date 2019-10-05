d54309dda1298254dc941f64a3154cdf152e8ae8
mr04vv <mr04vv@gmail.com>
2019-10-05 13:43:24 +0900

    youtube

--- src/pages/Top.tsx
+++ src/pages/Top.tsx
@@ -1,8 +1,30 @@
 import * as React from 'react';
+import ReactPlayer from 'react-player';
 
 const Top = () => (
   <div>
-    わいわいの面白シーンを共有しよう！
+    わいわいの好きなシーンを共有しよう！
+    <div>
+      <ReactPlayer
+        width="100%"
+        url="https://www.youtube.com/watch?v=b8M2TwcHqfs&t=986s"
+        youtubeConfig={{ playerVars: { end: 992 } }}
+      />
+      <ReactPlayer
+        width="100%"
+        url="https://www.youtube.com/watch?v=haJOSvjughw&t=805s"
+        youtubeConfig={{ playerVars: { end: 820 } }}
+      />
+      <ReactPlayer width="100%" url="https://www.youtube.com/watch?v=b8M2TwcHqfs&t=986s" />
+      <ReactPlayer width="100%" url="https://www.youtube.com/watch?v=b8M2TwcHqfs&t=986s" />
+      <ReactPlayer width="100%" url="https://www.youtube.com/watch?v=b8M2TwcHqfs&t=986s" />
+
+      {/* <ReactPlayer url="https://www.youtube.com/watch?v=b8M2TwcHqfs&t=986s" /> */}
+      {/* <ReactPlayer url="https://www.youtube.com/watch?v=b8M2TwcHqfs&t=986s" /> */}
+      {/* <ReactPlayer url="https://www.youtube.com/watch?v=b8M2TwcHqfs&t=986s" /> */}
+      {/* <ReactPlayer url="https://www.youtube.com/watch?v=b8M2TwcHqfs&t=986s" /> */}
+      {/* <ReactPlayer url="https://www.youtube.com/watch?v=b8M2TwcHqfs&t=986s" /> */}
+    </div>
   </div>
 );
 
