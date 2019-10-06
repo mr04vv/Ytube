78ebecc350766793add940c17f2ac6f9528c5dfe
mr04vv <mr04vv@gmail.com>
2019-10-07 04:12:54 +0900

    feat: create post

--- src/hooks/Post/usePost.ts
+++ src/hooks/Post/usePost.ts
@@ -1,8 +1,13 @@
 /* eslint-disable no-console */
 import {
-  useState,
+  useState, useEffect,
 } from 'react';
+import { CategoryInterface } from 'interfaces/CategoryInterface';
+import { GameInterface } from 'interfaces/GameInterface';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchGames } from 'reduxes/modules/games/gameList';
 
+import { fetchCategories } from 'reduxes/modules/categories/categoryList';
 
 const usePost = () => {
   const [startTime, setStartTime] = useState<string>('0');
@@ -11,10 +16,47 @@ const usePost = () => {
   const [tabIndex, setTabIndex] = useState<number>(1);
   const [error, setError] = useState<string>('');
   const [title, setTitle] = useState<string>('');
-  const [titleError, setTitleError] = useState<string>('');
+  const [titleError] = useState<string>('');
   const [comment, setComment] = useState<string>('');
-  const [commentError, setCommentError] = useState<string>('');
-  const [game, setGame] = useState<any>(10);
+  const [commentError] = useState<string>('');
+  const [game, setGame] = useState<number | undefined>();
+  const [category, setCategory] = useState<number[]>([]);
+  const [categoryMaster, setCategoryMaster] = useState<CategoryInterface[]>([]);
+  const [gameMaster, setGameMaster] = useState<GameInterface[]>([]);
+  const [categoryName, setCategoryName] = useState<string[]>([]);
+  const gameSelector = (state: any) => state.gameList;
+  const gameState = useSelector(gameSelector);
+  const categorySelector = (state: any) => state.categoryList;
+  const categoryState = useSelector(categorySelector);
+  const dispatch = useDispatch();
+
+  const init = () => {
+    setTitle('');
+    setCategory([]);
+    setCategoryName([]);
+    setGame(undefined);
+    setComment('');
+    setUrl('');
+    setStartTime('0');
+    setEndTime('0');
+  };
+
+  useEffect(() => {
+    dispatch(fetchCategories());
+    dispatch(fetchGames());
+  }, []);
+
+  useEffect(() => {
+    if (categoryState.data.length > 0) {
+      setCategoryMaster(categoryState.data);
+    }
+  }, [categoryState.data]);
+
+  useEffect(() => {
+    if (gameState.data.length > 0) {
+      setGameMaster(gameState.data);
+    }
+  }, [gameState.data]);
 
   const setStart = (r: any) => {
     if (r.player) {
@@ -33,7 +75,9 @@ const usePost = () => {
   };
 
   const post = () => {
-
+    init();
+    console.debug(game);
+    console.debug(category);
   };
 
 
@@ -60,7 +104,20 @@ const usePost = () => {
     },
     commentError,
     game,
-    setGame,
+    setGame: (v: string) => {
+      setGame(parseInt(v, 10));
+    },
+    gameMaster,
+    categoryMaster,
+    category,
+    setCategory: (v: string[]) => {
+      const list = categoryMaster.filter((c: CategoryInterface) => v.includes(c.name));
+      const idList = list.map((l: CategoryInterface) => l.id);
+      setCategory(idList);
+      setCategoryName(v);
+    },
+    categoryName,
+    post,
   };
 };
 
