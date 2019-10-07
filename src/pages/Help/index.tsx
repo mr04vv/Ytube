215850a9266317f25799e64e11c37ccc9838460e
mr04vv <mr04vv@gmail.com>
2019-10-08 05:39:55 +0900

    fix: scroll top help page

--- src/pages/Help/index.tsx
+++ src/pages/Help/index.tsx
@@ -7,88 +7,90 @@ const urlHelp = require('images/url.jpg');
 const setHelp = require('images/setting.jpg');
 const content = require('images/content.jpg');
 
-window.scrollTo(0, 0);
+const Help = () => {
+  window.scrollTo(0, 0);
 
-const Help = () => (
-  <>
-    <Container>
-      <Title>
-        使い方
-      </Title>
-      <PromptContainer>
-        <div>投稿機能の利用にはログインが必要です</div>
-      </PromptContainer>
-      <Explain>
-        中央下部の＋ボタンを押します
-      </Explain>
-      <ImageContainer width="80%" src={spHelp} alt="" />
+  return (
+    <>
+      <Container>
+        <Title>
+          使い方
+        </Title>
+        <PromptContainer>
+          <div>投稿機能の利用にはログインが必要です</div>
+        </PromptContainer>
+        <Explain>
+          中央下部の＋ボタンを押します
+        </Explain>
+        <ImageContainer width="80%" src={spHelp} alt="" />
 
-      <Explain>
-        投稿したい動画のURLを貼り付けます
-      </Explain>
-      <ImageContainer width="80%" src={urlHelp} alt="" />
+        <Explain>
+          投稿したい動画のURLを貼り付けます
+        </Explain>
+        <ImageContainer width="80%" src={urlHelp} alt="" />
 
-      <ExplainContent>
-        動画を再生し、お好みの場所で開始時間と終了時間をセットし右上の次へボタンを押します
-      </ExplainContent>
-      <ImageContainer width="80%" src={setHelp} alt="" />
+        <ExplainContent>
+          動画を再生し、お好みの場所で開始時間と終了時間をセットし右上の次へボタンを押します
+        </ExplainContent>
+        <ImageContainer width="80%" src={setHelp} alt="" />
 
-      <ExplainContent>
-        1. タイトルを入力します
-        <br />
-        2. 内容を入力します
-        <br />
-        3. ゲームとカテゴリを選択します
-        <br />
-        4. ゲーム、カテゴリは自由に追加できます
-        <br />
-        5. 右上の投稿ボタンを押して投稿
-      </ExplainContent>
-      <ImageContainer width="80%" src={content} alt="" />
-    </Container>
-    <FooterContainer>
-      <Title>
-        わいわいさんリンク
-      </Title>
-      <SNSLink target="_brank" href="https://www.youtube.com/channel/UCSkLRGGIGKOtinamhcy_42g">[YouTube]YY-CHANNEL [YCH]</SNSLink>
-      <SNSLink target="_brank" href="https://twitter.com/ABCDYY">[Twitter]わいわい(31)</SNSLink>
-      <SNSLink target="_brank" href="https://www.instagram.com/waiwai_31yearsold/?hl=ja">[Instagram]わいわい(31)</SNSLink>
-      <Title>
-        運営より
-      </Title>
-      <Text>
-        本サイトの運営には月1,000円ほどかかっています(2019/10/8現在)。本サイトを今後も継続していく為、皆様のご支援をよろしくおねがいします。
-      </Text>
-      <SNSLink target="_brank" href="https://www.amazon.co.jp/hz/wishlist/ls/J7CJ3KNEL2PF">サイトを支援する</SNSLink>
-      <SNSLink target="_brank" href="https://twitter.com/_mooriii">作者Twitterリンク</SNSLink>
-      <SNSLink target="_brank" href="https://twitter.com/akkun_jiro">作者Twitterリンク２</SNSLink>
-      <SNSLink target="_brank" href="https://mooriii.com/">作者ホームページ</SNSLink>
-      <SNSLink target="_brank" href="https://github.com/mr04vv/Ytube">ソースコード</SNSLink>
-      <Text>その他ご要望等がある場合TwiiterのDMにて受け付けております。ご気軽にお申し付けください。</Text>
-      <Share>ツイッターでこのサイトを共有する</Share>
-      <Share>↓↓↓↓↓↓</Share>
-      <TwitterShareButton
-        url="https://ytube-938fd.firebaseapp.com/home"
-        title="YtubeでYYさんの好きなシーンを共有しよう！"
-      >
-        <TwitterIcon
-          size={32}
-          round
-        />
-      </TwitterShareButton>
-      <Attention>
-        Created by
-        {' '}
-        <a target="_brank" href="https://mooriii.com/">Takuto Mori</a>
-      </Attention>
-      <Attention>
-        当サイトでは、アクセス解析のためにGoogleアナリティクスを使用しています。 このGoogleアナリティクスはトラフィックデータの収集のためにCookieを使用しています。 このトラフィックデータは匿名で収集されており、個人を特定するものではありません。 この機能はCookieを無効にすることで収集を拒否することが出来ますので、お使いのブラウザの設定をご確認ください。 なおこの規約に関しまして、詳細は
-        <a target="_brank" href="https://marketingplatform.google.com/about/analytics/terms/jp/">Googleアナリティクス利用規約</a>
-        にてご確認ください。
-      </Attention>
-    </FooterContainer>
-  </>
-);
+        <ExplainContent>
+          1. タイトルを入力します
+          <br />
+          2. 内容を入力します
+          <br />
+          3. ゲームとカテゴリを選択します
+          <br />
+          4. ゲーム、カテゴリは自由に追加できます
+          <br />
+          5. 右上の投稿ボタンを押して投稿
+        </ExplainContent>
+        <ImageContainer width="80%" src={content} alt="" />
+      </Container>
+      <FooterContainer>
+        <Title>
+          わいわいさんリンク
+        </Title>
+        <SNSLink target="_brank" href="https://www.youtube.com/channel/UCSkLRGGIGKOtinamhcy_42g">[YouTube]YY-CHANNEL [YCH]</SNSLink>
+        <SNSLink target="_brank" href="https://twitter.com/ABCDYY">[Twitter]わいわい(31)</SNSLink>
+        <SNSLink target="_brank" href="https://www.instagram.com/waiwai_31yearsold/?hl=ja">[Instagram]わいわい(31)</SNSLink>
+        <Title>
+          運営より
+        </Title>
+        <Text>
+          本サイトの運営には月1,000円ほどかかっています(2019/10/8現在)。本サイトを今後も継続していく為、皆様のご支援をよろしくおねがいします。
+        </Text>
+        <SNSLink target="_brank" href="https://www.amazon.co.jp/hz/wishlist/ls/J7CJ3KNEL2PF">サイトを支援する</SNSLink>
+        <SNSLink target="_brank" href="https://twitter.com/_mooriii">作者Twitterリンク</SNSLink>
+        <SNSLink target="_brank" href="https://twitter.com/akkun_jiro">作者Twitterリンク２</SNSLink>
+        <SNSLink target="_brank" href="https://mooriii.com/">作者ホームページ</SNSLink>
+        <SNSLink target="_brank" href="https://github.com/mr04vv/Ytube">ソースコード</SNSLink>
+        <Text>その他ご要望等がある場合TwiiterのDMにて受け付けております。ご気軽にお申し付けください。</Text>
+        <Share>ツイッターでこのサイトを共有する</Share>
+        <Share>↓↓↓↓↓↓</Share>
+        <TwitterShareButton
+          url="https://ytube-938fd.firebaseapp.com/home"
+          title="YtubeでYYさんの好きなシーンを共有しよう！"
+        >
+          <TwitterIcon
+            size={32}
+            round
+          />
+        </TwitterShareButton>
+        <Attention>
+          Created by
+          {' '}
+          <a target="_brank" href="https://mooriii.com/">Takuto Mori</a>
+        </Attention>
+        <Attention>
+          当サイトでは、アクセス解析のためにGoogleアナリティクスを使用しています。 このGoogleアナリティクスはトラフィックデータの収集のためにCookieを使用しています。 このトラフィックデータは匿名で収集されており、個人を特定するものではありません。 この機能はCookieを無効にすることで収集を拒否することが出来ますので、お使いのブラウザの設定をご確認ください。 なおこの規約に関しまして、詳細は
+          <a target="_brank" href="https://marketingplatform.google.com/about/analytics/terms/jp/">Googleアナリティクス利用規約</a>
+          にてご確認ください。
+        </Attention>
+      </FooterContainer>
+    </>
+  );
+};
 
 export default Help;
 
