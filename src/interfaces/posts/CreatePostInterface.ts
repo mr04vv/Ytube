79a8ea74b071033d51a15085bc350b38a1f3a6e0
mr04vv <mr04vv@gmail.com>
2019-10-07 17:26:58 +0900

    feat: post

---
+++ src/interfaces/posts/CreatePostInterface.ts
@@ -0,0 +1,10 @@
+/* eslint-disable camelcase */
+export interface CreatePostInterface {
+  title: string;
+  detail: string;
+  start_time: number;
+  end_time: number;
+  video_url: string;
+  game_id: number;
+  category_ids: number[];
+}
