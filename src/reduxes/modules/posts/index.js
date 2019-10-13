8ee1a67d0fed5a4830fa6744a4901e497fa9c8ad
mr04vv <mr04vv@gmail.com>
2019-10-13 20:12:22 +0900

    feat: search

--- src/reduxes/modules/posts/index.js
+++ src/reduxes/modules/posts/index.js
@@ -2,7 +2,8 @@ import post from './post';
 import fetchPost from './fetchPost';
 import fetchPostMe from './fetchMyPost';
 import likedPost from './fetchLikedPost';
+import search from './searchPost';
 
 export {
-  post, fetchPost, fetchPostMe, likedPost,
+  post, fetchPost, fetchPostMe, likedPost, search,
 };
