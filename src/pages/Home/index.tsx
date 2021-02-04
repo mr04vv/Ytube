14f2ce0eb4be6532b2c827e03d2b6c0c6ba60fd4
TakutoMori <mr04vv@gmail.com>
2021-02-05 00:14:21 +0900

    wip: 表示テスト

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -19,6 +19,8 @@ import {
 import { SearchOutlined, TuneOutlined } from '@material-ui/icons';
 import { CategoryInterface } from 'interfaces/CategoryInterface';
 import { GameInterface } from 'interfaces/GameInterface';
+import { PostListItem } from 'components/PostListItem';
+import { Post } from 'entity/entity/post';
 import {
   SearchTextField,
   SearchButton,
@@ -32,15 +34,35 @@ import {
 import PostModal from './PostModal';
 
 const Home = () => {
-  const modalOpen = usePostModal();
-  const master = useMasterData();
-  const post = useFetchPost(master.categoryMaster, master.gameMaster);
-  const info = useMyInfo();
+  // const modalOpen = usePostModal();
+  // const master = useMasterData();
+  // const post = useFetchPost(master.categoryMaster, master.gameMaster);
+  // const info = useMyInfo();
   const [isExpanded, setIsExpanded] = React.useState<boolean>(false);
+
+  const posts = { posts: [{ id: 2046, title: 'フィル、うごめきます。', videoUrl: 'https://www.youtube.com/watch?v=e8i1gnsvqJE\u0026feature=share', detail: '', endTime: 107, startTime: 90, createdAt: '2020-08-13T14:29:57.000+09:00', isAnonymous: true, likeCount: 218, alreadyLiked: null, thumbnailUrl: 'https://i.ytimg.com/vi/e8i1gnsvqJE/hqdefault.jpg', playCount: 2248, dynamicLink: 'https://yycollection.page.link/8CgCsPyDQ8iT66uq9', user: { id: 117, name: 'меї', imageUrl: 'https://pbs.twimg.com/profile_images/1276718040764149760/IdNJk0hb.jpg' }, game: { id: 1, title: 'Patrol Duty' }, categories: [{ id: 1, name: '発作' }] }, { id: 2171, title: 'なむあみだぁぶつ', videoUrl: 'https://youtu.be/Tx3ApbqHfeE', detail: '個人的に1番おもろいと思ってる雑談。', endTime: 1425, startTime: 1291, createdAt: '2020-08-15T09:51:57.000+09:00', isAnonymous: false, likeCount: 146, alreadyLiked: null, thumbnailUrl: 'https://i.ytimg.com/vi/Tx3ApbqHfeE/hqdefault.jpg', playCount: 1402, dynamicLink: 'https://yycollection.page.link/23QMFNA88TSUageS8', user: { id: 2791, name: 'るなるな', imageUrl: 'https://lh3.googleusercontent.com/a-/AOh14Gju4cwzHIH7kCopoNy2BfViyK4axoKjzizcOLZlcw=s96-c' }, game: { id: 69, title: '生放送' }, categories: [{ id: 5, name: '雑談' }] }, { id: 2042, title: '突然の来客', videoUrl: 'https://www.youtube.com/watch?v=H2TGcLygTcM\u0026feature=share', detail: '小さい会話の中にも面白さが詰まっています', endTime: 358, startTime: 260, createdAt: '2020-08-13T12:52:39.000+09:00', isAnonymous: false, likeCount: 138, alreadyLiked: null, thumbnailUrl: 'https://i.ytimg.com/vi/H2TGcLygTcM/hqdefault.jpg', playCount: 1844, dynamicLink: 'https://yycollection.page.link/dXQLSfyuWpuCKf5m7', user: { id: 1451, name: 'タカ@ガヤ民 ぷいきゅあ研修生', imageUrl: 'https://pbs.twimg.com/profile_images/1257222256230494214/9vyoRWh3.jpg' }, game: { id: 38, title: 'LOST EGG' }, categories: [{ id: 32, name: 'びっくり' }, { id: 2, name: 'コント' }] }, { id: 2056, title: '「バスビバーﾝスビバスビバン」', videoUrl: 'https://youtu.be/3mR9Lg3fK90', detail: '', endTime: 157, startTime: 132, createdAt: '2020-08-13T17:21:32.000+09:00', isAnonymous: false, likeCount: 128, alreadyLiked: null, thumbnailUrl: 'https://i.ytimg.com/vi/3mR9Lg3fK90/hqdefault.jpg', playCount: 1197, dynamicLink: 'https://yycollection.page.link/oXwL8Wy7Wt9vdSPdA', user: { id: 1791, name: 'らっきょ。', imageUrl: 'https://pbs.twimg.com/profile_images/1307322353135706112/6bTxVWhZ.jpg' }, game: { id: 48, title: "Five Nights at Freddy's" }, categories: [{ id: 1, name: '発作' }] }, { id: 2128, title: '君はロックを聞かない', videoUrl: 'https://www.youtube.com/watch?v=Envk9KpadDE\u0026feature=share', detail: '', endTime: 1561, startTime: 1530, createdAt: '2020-08-14T13:45:44.000+09:00', isAnonymous: false, likeCount: 114, alreadyLiked: null, thumbnailUrl: 'https://i.ytimg.com/vi/Envk9KpadDE/hqdefault.jpg', playCount: 916, dynamicLink: 'https://yycollection.page.link/VKBbZhtq2iAKdhdP7', user: { id: 2581, name: 'にぼし', imageUrl: 'https://pbs.twimg.com/profile_images/1263440823200309250/zRyn7UTl.jpg' }, game: { id: 86, title: 'BIOHAZARD RE:2' }, categories: [{ id: 6, name: '歌' }] }, { id: 2114, title: 'ヤメロヨォ！！！(クソデカボイス)', videoUrl: 'https://youtu.be/NvPDzuDA-u0', detail: '音量注意', endTime: 2651, startTime: 2607, createdAt: '2020-08-14T06:30:38.000+09:00', isAnonymous: false, likeCount: 96, alreadyLiked: null, thumbnailUrl: 'https://i.ytimg.com/vi/NvPDzuDA-u0/hqdefault.jpg', playCount: 1318, dynamicLink: 'https://yycollection.page.link/3mRv24PUTM4UStHJ8', user: { id: 2231, name: '王白石', imageUrl: 'https://pbs.twimg.com/profile_images/1201254396522184704/dUMKeOkm.jpg' }, game: { id: 591, title: 'FALL GUYS ULTIMATE KNOCKOUT' }, categories: [{ id: 61, name: 'がんばり' }, { id: 161, name: 'ツボ' }, { id: 10, name: '生放送' }, { id: 36, name: '鼓膜破裂' }, { id: 20, name: '怒り' }] }, { id: 5421, title: 'アンダーザシー ', videoUrl: 'https://www.youtube.com/watch?v=_w5wq68eec8\u0026feature=share', detail: '生放送の方よりこっちの方が好きです', endTime: 1782, startTime: 1646, createdAt: '2020-09-06T23:39:21.000+09:00', isAnonymous: false, likeCount: 95, alreadyLiked: null, thumbnailUrl: 'https://i.ytimg.com/vi/_w5wq68eec8/hqdefault.jpg', playCount: 945, dynamicLink: 'https://yycollection.page.link/TtQW6172Xaod1nMC7', user: { id: 16491, name: 'pickles', imageUrl: 'https://lh6.googleusercontent.com/-0i78bJvenwQ/AAAAAAAAAAI/AAAAAAAAAAA/AMZuucmUKaJwUXIN3odxDhmuaY5hIQtzRw/s96-c/photo.jpg' }, game: { id: 4, title: 'Two Point Hospital' }, categories: [{ id: 601, name: 'おもしろ' }, { id: 281, name: '楽しそうなわいわい' }, { id: 5, name: '雑談' }, { id: 421, name: '笑い' }, { id: 6, name: '歌' }] }, { id: 6371, title: '記憶に新しい、過去1笑ったシーン', videoUrl: 'https://www.youtube.com/watch?v=SjNj8B8p4Hc\u0026feature=share', detail: 'これ、堪えられる人いるの？笑笑', endTime: 1110, startTime: 1005, createdAt: '2020-09-07T04:37:07.000+09:00', isAnonymous: false, likeCount: 94, alreadyLiked: null, thumbnailUrl: 'https://i.ytimg.com/vi/SjNj8B8p4Hc/hqdefault.jpg', playCount: 1215, dynamicLink: 'https://yycollection.page.link/WvGHPywyF5Ua7Qjf8', user: { id: 19611, name: 'ジョンソンボデーケア', imageUrl: 'https://abs.twimg.com/sticky/default_profile_images/default_profile.png' }, game: { id: 1071, title: 'The Last of Us Part II' }, categories: [{ id: 15, name: 'ツッコミ' }, { id: 161, name: 'ツボ' }, { id: 21, name: '実況' }, { id: 11, name: '爆笑' }, { id: 421, name: '笑い' }] }, { id: 407, title: 'いえあ　だっくしーずん　ならぱぁんでぇす', videoUrl: 'https://youtu.be/FLvuJ-wk_t0', detail: 'ビブラート効いてるね', endTime: 1586, startTime: 1569, createdAt: '2020-01-18T09:23:00.000+09:00', isAnonymous: false, likeCount: 93, alreadyLiked: null, thumbnailUrl: 'https://i.ytimg.com/vi/FLvuJ-wk_t0/hqdefault.jpg', playCount: 4129, dynamicLink: 'https://yycollection.page.link/djRGNCR56fXeaQtJA', user: { id: 116, name: 'Rちゃん', imageUrl: 'https://lh3.googleusercontent.com/a-/AAuE7mAsYXsB87_RF2TGEh5oLvvnGqi7qKkkUDpsy0zqFDc' }, game: { id: 53, title: 'Duck Season' }, categories: [] }, { id: 6931, title: 'ちょ、なにしてんのって、なにしてんのって自分ら！', videoUrl: 'https://www.youtube.com/watch?v=ukWTmkD3gt0\u0026feature=share', detail: 'おいたんが焦る瞬間。\n初めて見たら笑い止まらなくなる', endTime: 604, startTime: 544, createdAt: '2020-09-07T12:26:32.000+09:00', isAnonymous: false, likeCount: 86, alreadyLiked: null, thumbnailUrl: 'https://i.ytimg.com/vi/ukWTmkD3gt0/hqdefault.jpg', playCount: 746, dynamicLink: 'https://yycollection.page.link/Xbf9kDDzZQb8a3x5A', user: { id: 21381, name: 'たれぱんだ', imageUrl: 'https://lh4.googleusercontent.com/-GiDJTUgm7Nc/AAAAAAAAAAI/AAAAAAAAAAA/AMZuuckK6UtsDtA504yuvwaEGrq1ZdrbUg/s96-c/photo.jpg' }, game: { id: 731, title: 'Knoddskogen' }, categories: [{ id: 15, name: 'ツッコミ' }] }] };
 
   return (
     <>
-      <SearchContainer>
+      <div style={{
+        margin: '80px 16px',
+        display: 'flex',
+        justifyContent: 'space-around',
+        flexWrap: 'wrap'
+      }}
+      >
+        {posts.posts.map(post => <div style={{
+          margin: '10px ',
+          width: '23%',
+          minWidth: '230px'
+        }}
+        >
+          <PostListItem post={post as any} />
+
+        </div>)}
+
+      </div>
+      {/* <SearchContainer>
         <FormControl
           fullWidth
           style={{
@@ -185,7 +207,7 @@ const Home = () => {
             per={post.per}
           />
         </Container>
-      )}
+      )} */}
     </>
   );
 };
