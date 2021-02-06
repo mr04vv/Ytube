68fef44778f08d5ee86900a9202b344bc0ec4bc8
TakutoMori <mr04vv@gmail.com>
2021-02-06 17:05:35 +0900

    chore: typescriptバージョンアップ

--- src/pages/Home/PostModal/styles.ts
+++ src/pages/Home/PostModal/styles.ts
@@ -122,10 +122,21 @@ const NewPostContainer = styled.div`
   margin: 0 auto;
 `;
 
-const AddButton = styled(CustomButton)`
+const AddButton = styled(Button)`
   margin-top: 16px;
   margin-bottom: 8px;
   padding-bottom: 3px;
+  text-transform: unset;
+  background-color: #ffbd14;
+  border-radius: 0;
+  color: black;
+  box-shadow: unset;
+  :hover {
+    background-color: #ffbd14;
+    opacity: 0.7;
+    color: black;
+    box-shadow: unset;
+  }
 `;
 
 export {
