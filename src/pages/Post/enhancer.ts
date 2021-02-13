9f708f1412b574fb34d27ccdc1415f82cd1dae26
TakutoMori <mr04vv@gmail.com>
2021-02-14 01:57:16 +0900

    feat: 投稿詳細画面実装

--- src/pages/Post/enhancer.ts
+++ src/pages/Post/enhancer.ts
@@ -1,31 +1,53 @@
 /* eslint-disable no-console */
 import { useState, useEffect } from 'react';
-import { PostInterface } from 'interfaces/posts/PostInterface';
 import fetchPost from 'api/posts/fetchPost';
 import useReactRouter from 'use-react-router';
+import { RouteComponentProps } from 'react-router';
+import { Post } from 'entity/entity/post';
+import { fetchRandomPostList } from 'api/posts/fetchRandomPostList';
 
-export interface OrderInterface {
-  id: number;
-  name: string;
-}
-
-const useFetchPost = () => {
-  const [posts, setPosts] = useState<PostInterface[]>([]);
+export const useEnhancer = () => {
+  const [post, setPost] = useState<Post>();
+  const [randomPosts, setRandomPosts] = useState<Post[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
-  const { match } = useReactRouter();
-  const { params }: any = match;
+  const { match, history }: RouteComponentProps<{id: string}> = useReactRouter();
+  const { params } = match;
+
+  const [failed, setFailed] = useState<boolean>(false);
+
   useEffect(() => {
-    (async () => {
-      const post = await fetchPost(params.id);
-      setPosts([post as any]);
+    const postIdStr = params.id;
+    const postId = Number(postIdStr);
+    if (Number.isNaN(postId)) {
       setIsLoading(false);
-    })();
+      setFailed(true);
+    } else {
+      (async () => {
+        try {
+          const [postRes, randomRes] = await Promise.all([fetchPost(postId), fetchRandomPostList()]);
+          setPost(postRes.post);
+          setRandomPosts(randomRes.posts);
+        } finally {
+          setIsLoading(false);
+        }
+      })();
+    }
   }, [params.id]);
 
+  const pushPostDetailPage = (postId: number) => {
+    setIsLoading(true);
+    window.scrollTo(0, 0);
+    history.push({
+      pathname: `/post/${postId}`,
+    });
+  };
+
+
   return {
-    posts,
+    post,
     isLoading,
+    failed,
+    randomPosts,
+    pushPostDetailPage
   };
 };
-
-export default useFetchPost;
